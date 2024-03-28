83e65a60447df6e8a49bd179bdecbc8187db987f
한정수 <wjdtn1642@naver.com>
2024-03-29 00:27:51 +0900

    네이버를 예시로 웹 스크래핑 파악 중

--- wep.py
+++ wep.py
@@ -9,3 +9,7 @@ response = requests.get(url)
 
 # HTML 파싱
 soup = BeautifulSoup(response.text, 'html.parser')
+
+# 실시간 검색어 순위 추출
+for rank, keyword in enumerate(soup.select('.PM_CL_realtimeKeyword_rolling span[class*=ah_k]'), 1):
+    print(f'{rank}위: {keyword.get_text()}')

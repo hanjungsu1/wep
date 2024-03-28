e546b5c9dcab0b146235622195c5bb00743eaca0
한정수 <wjdtn1642@naver.com>
2024-03-29 02:40:32 +0900

    검색어 출력 실패시 이벤트 작성

--- wep.py
+++ wep.py
@@ -10,6 +10,14 @@ response = requests.get(url)
 # HTML 파싱
 soup = BeautifulSoup(response.text, 'html.parser')
 
-# 실시간 검색어 순위 추출
-for rank, keyword in enumerate(soup.select('.PM_CL_realtimeKeyword_rolling span[class*=ah_k]'), 1):
-    print(f'{rank}위: {keyword.get_text()}')
+# 실시간 검색어가 있는 부분의 HTML 태그와 클래스를 확인하여 찾기
+realtime_search = soup.find('ol', class_='lst_realtime_srch')
+
+# 검색어를 가져와 출력
+if realtime_search:
+    print("네이버 실시간 검색어 순위:")
+    # 실시간 검색어 순위 추출
+    for rank, keyword in enumerate(soup.select('.PM_CL_realtimeKeyword_rolling span[class*=ah_k]'), 1):
+        print(f'{rank}위: {keyword.get_text()}')
+else:
+    print("실시간 검색어를 찾을 수 없습니다.")

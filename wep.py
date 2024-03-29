cfba630b6aed2159bde84a58c16dc341b8f579f5
한정수 <wjdtn1642@naver.com>
2024-03-29 22:13:58 +0900

    잘못된 주소 오류를 해결하기 위해 네이버에서 다음 사이트로 변경

--- wep.py
+++ wep.py
@@ -2,17 +2,17 @@ import feedparser
 import tkinter as tk
 
 def scrape_and_display():
-    # 네이버 뉴스 RSS 피드 URL
-    rss_url = 'https://news.naver.com/main/home.naver/rss.nhn'
+    # Daum 뉴스 RSS 피드 URL
+    rss_url = "http://media.daum.net/syndication/today_sisa.rss"
 
     # 웹 페이지 가져오기
     response = feedparser.parse(rss_url)
 
-    # HTML 파싱
-    soup = BeautifulSoup(response.text, 'html.parser')
+    # 뉴스 헤드라인 리스트 초기화
+    headlines = []
 
     # 각 뉴스 헤드라인을 리스트에 추가
-    for entry in feed.entries:
+    for entry in response.entries:
         headlines.append(entry.title)
 
     # 리스트에 있는 뉴스 헤드라인을 라벨에 출력
@@ -25,7 +25,7 @@ root = tk.Tk()
 root.title(" 뉴스 헤드라인")
 
 # 스크래핑 및 화면 출력 버튼 생성
-scrape_button = tk.Button(root, text="네이버 뉴스 헤드라인 가져오기", command=scrape_and_display)
+scrape_button = tk.Button(root, text="Daum 뉴스 헤드라인 가져오기", command=scrape_and_display)
 scrape_button.pack()
 
 # 윈도우 실행

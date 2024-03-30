173ed7e7a94f15e0bcd7554bfd64ffbe36f4b3ae
한정수 <wjdtn1642@naver.com>
2024-03-30 19:46:59 +0900

    단순히 헤드라인을 보여주는 것이 아닌, 하이퍼링크로 해당 기사 사이트를 직접 볼 수 있게 변경, 하지만 원하는 결과는 아직 보이지 않음

--- wep.py
+++ wep.py
@@ -1,24 +1,33 @@
 import feedparser
 import tkinter as tk
+import webbrowser
+
+def open_news_link(likn):
+    webbrowser.open(link)
 
 def scrape_and_display():
     # Daum 뉴스 RSS 피드 URL
-    rss_url = "http://media.daum.net/syndication/today_sisa.rss"
+    url = "http://media.daum.net/syndication/today_sisa.rss"
 
     # 웹 페이지 가져오기
-    response = feedparser.parse(rss_url)
+    response = feedparser.parse(url)
 
     # 뉴스 헤드라인 리스트 초기화
     headlines = []
 
     # 각 뉴스 헤드라인을 리스트에 추가
     for entry in response.entries:
-        headlines.append(entry.title)
+        headlines = entry.title
+        link = entry.link
+        headlines.append((headline, link))
 
     # 리스트에 있는 뉴스 헤드라인을 라벨에 출력
     for idx, headline in enumerate(headlines):
-        label = tk.Label(root, text=f"{idx + 1}. {headline}")
+        label = tk.Label(root, text=f"{idx + 1}. {headline}", fg="blue", cursor="hand2")
+        label.bind("<Button-1>", lambda e, link=link: open_news_link(link))
         label.pack()
+    
+
 
 # Tkinter 윈도우 생성
 root = tk.Tk()

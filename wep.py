ccbfd3a2f3d017a842d603a20c11d7de2dcecee5
한정수 <wjdtn1642@naver.com>
2024-04-01 01:51:11 +0900

    결과 출력을 위해서 임포트를 urllib과 bs4로 변경

--- wep.py
+++ wep.py
@@ -1,24 +1,18 @@
-import feedparser
-import tkinter as tk
-import webbrowser
+# import feedparser
+# import webbrowser
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
 
-def scrape_and_display():
-    # Daum 뉴스 RSS 피드 URL
-    url = "http://media.daum.net/syndication/today_sisa.rss"
 
-    # 웹 페이지 가져오기
-    response = feedparser.parse(url)
+# Daum 뉴스 RSS 피드 URL
+url = urlopen("http://media.daum.net")
 
-    # 리스트에 있는 뉴스 헤드라인을 라벨에 출력
-    for idx, headline in enumerate(response.entries):
-        headline = entry.title
-        link = entry.link
-        print(f"{idx + 1}. {entry.title}")
-        print(f"    Link: {link}")
-        print()
+# 웹 페이지 가져오기
+response = BeautifulSoup(url, "html.parser")
 
-        # 기사 링크를 웹 브라우저에서 열기
-        webbrowser.open(link)
-    
-# 스크래핑 함수 호출
-scrape_and_display()
+# 리스트에 있는 뉴스 헤드라인을 라벨에 출력
+for link in response.find_all('a'):
+    print(link.text.strip(), link.get('herf'))
+
+for link in response.find_all('img'):
+    print(link.text.strip(), link.get('src'))
